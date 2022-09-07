f161a42e26efb70ce07892737cd9e1ea5aac66f3
davidmuir1999 <105323893+davidmuir1999@users.noreply.github.com>
2022-09-07 21:07:02 +0100

    Add files via upload

---
+++ Javascript beginner challenges/app.js
@@ -0,0 +1,62 @@
+//Question 1 create a function that returns the sum of two numbers
+// function addition(num1, num2){
+//     return  num1+num2
+// }
+// console.log(addition(2,3))
+
+//Question 2 create a functionthat converts hours into seconds
+// function timeConverter(hours){
+//     return hours * 3600
+// }
+// console.log(timeConverter(24))
+
+//Question 3 create a function that takes length and width of a rectangle and return its perimeter
+// function calcPerimeter(length,width){
+//     return (2*length) + (2*width)
+// }
+// console.log(calcPerimeter(6,7))
+
+//Question 4 write a function that takes the base and height of a triangle and retuns its area
+// function calcArea(base, height) {
+//     return (0.5 * (base)) * height
+// }
+// console.log(calcArea(3, 2))
+
+// Question 5 write a function that accepts a string and adds "frontend" onto the end of it
+// function extendString(string){
+//     console.log(`${string} Frontend`)
+// }
+// extendString("Welcome to")
+
+// Question 6 given two numbers, return true if the sum of both numbers is greater than 100. Otherwise return false
+// function numberGenerator(num1, num2) {
+//     return num1 + num2 >100
+// }
+
+// console.log(numberGenerator(50,1))
+
+// Question 7 wrute a function that accepts a number and returns true if it is less than or equal to zero, otherwise returns false
+// function lessThanOrEqualTo(num){
+//     return num <= 0
+// }
+// console.log(lessThanOrEqualTo(-1))
+
+// Question 8 given a boolean (true or false) return the opposite boolean
+// function oppositeBoolean(boolean){
+//     return !boolean;
+// }
+// console.log(oppositeBoolean(false))
+
+// Question 9 given any element, return true if it is NOT the number 0
+// function notZero(element){
+//     return element !== 0;
+// }
+
+// console.log(notZero(null))
+
+// Question 10 given two numbers return their reminder when divided by each other
+// function calcRemainder(num1, num2){
+//     return num1 % num2;
+// }
+
+// console.log(calcRemainder(7,8))
